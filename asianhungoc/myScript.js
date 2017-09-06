d832c065ea356234f8f3f68c0539832d602c6b69
Trung Nguyen <ngminhtrung@users.noreply.github.com>
2017-09-06 10:55:32 +0700

    techmaster

--- asianhungoc/myScript.js
+++ asianhungoc/myScript.js
@@ -4,7 +4,7 @@ const menuData = [
 	dishId: "v1",
 	dishCat: "pho-bun",
 	dishName: "Phở Bò",
-	dishPrice: "7,50 &euro;",
+	dishPrice: "7,50 €",
 	dishIngredient: "Phở tươi, thịt bò Kô-bê, nước xương bò, các loại gia vị và rau sống.",
 	dishPhotoDir: "photos/menu/menu-pho-v1.png"
 },
@@ -12,7 +12,7 @@ const menuData = [
 	dishId: "v2",
 	dishCat: "pho-bun",
 	dishName: "Phở Gà",
-	dishPrice: "7,00 &euro;",
+	dishPrice: "7,00 €",
 	dishIngredient: "Phở tươi, thịt gà đồi chạy bộ, nước xương gà, các loại gia vị và rau sống",
 	dishPhotoDir: "photos/menu/menu-pho-v2.png"
 },
@@ -20,7 +20,7 @@ const menuData = [
 	dishId: "v3",
 	dishCat: "pho-bun",
 	dishName: "Gỏi cuốn",
-	dishPrice: "3,90 &euro;",
+	dishPrice: "3,90 €",
 	dishIngredient: "Bánh đa, tôm tươi luộc bóc vỏ, hẹ, cà rốt thái mỏng, dứa thái mỏng, chuối non thái mỏng, nước chấm",
 	dishPhotoDir: "photos/menu/menu-bun-v3.png"
 },
@@ -28,7 +28,7 @@ const menuData = [
 	dishId: "v4",
 	dishCat: "pho-bun",
 	dishName: "Bún Thịt Nướng",
-	dishPrice: "7,90 &euro;",
+	dishPrice: "7,90 €",
 	dishIngredient: "Bún tươi, thịt miếng nướng than hoa, nước chấm bún thịt nướng, rau sống",
 	dishPhotoDir: "photos/menu/menu-bun-v4.png"
 },
@@ -37,7 +37,7 @@ const menuData = [
 	dishId: "v5",
 	dishCat: "pho-bun",
 	dishName: "Bún Chả Nướng",
-	dishPrice: "7,90 &euro;",
+	dishPrice: "7,90 €",
 	dishIngredient: "Bún tươi, thịt băm nướng than hoa, nước chấm bún thịt nướng, rau sống ",
 	dishPhotoDir: "photos/menu/menu-bun-v5.png"
 },
@@ -46,7 +46,7 @@ const menuData = [
 	dishId: "v6",
 	dishCat: "pho-bun",
 	dishName: "Bún Nem",
-	dishPrice: "7,90 &euro;",
+	dishPrice: "7,90 €",
 	dishIngredient: "Bún tươi, nem rán nhân thịt (lợn) băm, nấm tươi, xu hào, hành tây, cà rốt băm cnhỏ, nước chấm nem, rau sống",
 	dishPhotoDir: "photos/menu/menu-bun-v6.png"
 },
@@ -55,7 +55,7 @@ const menuData = [
 	dishId: "v7",
 	dishCat: "pho-bun",
 	dishName: "Bún Ba Loại Thịt",
-	dishPrice: "9,90 &euro;",
+	dishPrice: "9,90 €",
 	dishIngredient: "Bún tươi, thịt cừu và thịt lợn nướng, tôm nướng, nước chấm, rau sống.",
 	dishPhotoDir: "photos/menu/menu-bun-v7.png"
 },
@@ -64,7 +64,7 @@ const menuData = [
 	dishId: "v8",
 	dishCat: "pho-bun",
 	dishName: "Bún bò xào xả ớt",
-	dishPrice: "8,50 &euro;",
+	dishPrice: "8,50 €",
 	dishIngredient: "Bún tươi, thịt bò xào, lạc rang giã nhỏ, rau sống, nước trộn bún.",
 	dishPhotoDir: "photos/menu/menu-bun-v8.png"
 },
@@ -73,7 +73,7 @@ const menuData = [
 	dishId: "v9",
 	dishName: "Chè thập cẩm Như Ngọc đặc biệt",
 	dishCat: "drink",
-	dishPrice: "3,00 &euro;",
+	dishPrice: "3,00 €",
 	dishIngredient: "",
 	dishPhotoDir: "photos/menu/menu-che.png"
 },
@@ -82,7 +82,7 @@ const menuData = [
 	dishId: "v10",
 	dishCat: "drink",
 	dishName: "drink",
-	dishPrice: "&euro;",
+	dishPrice: "€",
 	dishIngredient: "",
 	dishPhotoDir: "photos/menu/menu-che.png"
 },
@@ -90,7 +90,7 @@ const menuData = [
 	dishId: "v11",
 	dishCat: "drink",
 	dishName: "drink",
-	dishPrice: "&euro;",
+	dishPrice: "€",
 	dishIngredient: "",
 	dishPhotoDir: "photos/menu/menu-che.png"
 },
@@ -98,7 +98,7 @@ const menuData = [
 	dishId: "a1",
 	dishCat: "com",
 	dishName: "Cơm Chay Gà Xào Rau Củ Quả",
-	dishPrice: "6,60 &euro;",
+	dishPrice: "6,60 €",
 	dishIngredient: "Cơm trắng Hải Hậu, gà xào dứa - cà chua - cà rốt, rau thơm, nước sốt.",
 	dishPhotoDir: "photos/menu/menu-com-a1.png"
 },
@@ -106,7 +106,7 @@ const menuData = [
 	dishId: "a2",
 	dishCat: "com",
 	dishName: "Cơm Gà Bao Bột",
-	dishPrice: "6,80 &euro;",
+	dishPrice: "6,80 €",
 	dishIngredient: "Cơm trắng Hải Hậu, đùi gà bao bột chiên, rau sống, nước sốt.",
 	dishPhotoDir: "photos/menu/menu-com-a2.png"
 },
@@ -114,7 +114,7 @@ const menuData = [
 	dishId: "a3",
 	dishCat: "com",
 	dishName: "Cơm Gà Da Chiên Giòn",
-	dishPrice: "6,80 &euro;",
+	dishPrice: "6,80 €",
 	dishIngredient: "Cơm trắng Hải Hậu, đùi gà và cánh gà chiên giòn, rau sống, nước sốt.",
 	dishPhotoDir: "photos/menu/menu-com-a3.png"
 },
@@ -122,7 +122,7 @@ const menuData = [
 	dishId: "a4",
 	dishCat: "com",
 	dishName: "Cơm Vịt Da Chiên Giòn",
-	dishPrice: "7,50 &euro;",
+	dishPrice: "7,50 €",
 	dishIngredient: "Cơm trắng Hải Hậu, vịt miếng chiên giòn, rau sống, nướng sốt. ",
 	dishPhotoDir: "photos/menu/menu-com-a4.png"
 },
@@ -130,7 +130,7 @@ const menuData = [
 	dishId: "a5",
 	dishCat: "com",
 	dishName: "Cơm Xào Thịt Bò",
-	dishPrice: "7,20 &euro;",
+	dishPrice: "7,20 €",
 	dishIngredient: "Cơm trắng Hải Hậu, thịt bò xào với rau cải, rau súp lơ và dứa, rau sống.",
 	dishPhotoDir: "photos/menu/menu-com-a5.png"
 },
@@ -138,7 +138,7 @@ const menuData = [
 	dishId: "a6",
 	dishCat: "com",
 	dishName: "Cơm chay rau củ quả",
-	dishPrice: "5,50 &euro;",
+	dishPrice: "5,50 €",
 	dishIngredient: "Cơm trắng Hải Hậu, rau củ quả xào.",
 	dishPhotoDir: "photos/menu/menu-com-a6.png"
 },
@@ -146,7 +146,7 @@ const menuData = [
 	dishId: "b1",
 	dishCat: "take-away",
 	dishName: "Hộp mì xào thịt bò và ớt chuông",
-	dishPrice: "3,00 &euro;",
+	dishPrice: "3,00 €",
 	dishIngredient: "Mì trứng, thịt bò, ớt chuông, hành tươi, hành tây.",
 	dishPhotoDir: "photos/menu/menu-box-1.png"
 },
@@ -154,7 +154,7 @@ const menuData = [
 	dishId: "b2",
 	dishCat: "take-away",
 	dishName: "Mì xào với rau ăn kèm nem Loempia",
-	dishPrice: "3,50 &euro;",
+	dishPrice: "3,50 €",
 	dishIngredient: "Mì trứng, giá, nem Loempia.",
 	dishPhotoDir: "photos/menu/menu-box-2.png"
 },
@@ -162,7 +162,7 @@ const menuData = [
 	dishId: "b3",
 	dishCat: "take-away",
 	dishName: "Mì xào tôm",
-	dishPrice: "3,80 &euro;",
+	dishPrice: "3,80 €",
 	dishIngredient: "Mì trứng, cà rốt, giá, tôm và trứng gà.",
 	dishPhotoDir: "photos/menu/menu-box-3.png"
 },
@@ -170,7 +170,7 @@ const menuData = [
 	dishId: "b4",
 	dishCat: "take-away",
 	dishName: "Mì xào thịt gà",
-	dishPrice: "4,50 &euro;",
+	dishPrice: "4,50 €",
 	dishIngredient: "Mì trứng, cà rốt, giá, thịt gà bao bột chiên giòn.",
 	dishPhotoDir: "photos/menu/menu-box-4.png"
 },
